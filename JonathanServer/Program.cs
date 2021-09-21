8a0f562eb73207aeb1bb5b7e128d6b1b8ddf1f79
JonathanGagaga <c7xiang@edu.uwaterloo.ca>
2021-09-21 11:28:03 -0400

    Add project files.

---
+++ JonathanServer/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JonathanServer
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            ServerControl server = new ServerControl();
+            server.Start();
+
+            Console.ReadKey();
+        }
+       
+    }
+
+}
---
+++ JonathanServer/ServerControl.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace JonathanServer
+{
+    public class ServerControl
+    {
+
+        private Socket serverSocket;
+        
+        private List<Socket> clientList;
+
+        public ServerControl()
+        {
+            serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            clientList = new List<Socket>();
+        }
+
+        public void Start()
+        {
+            
+            serverSocket.Bind(new IPEndPoint(IPAddress.Any, 22));
+            serverSocket.Listen(10);
+            Console.WriteLine("Server Starts Successfully");
+
+            
+            Thread threadAccept = new Thread(Accept);
+            threadAccept.IsBackground = true;
+            threadAccept.Start();
+        }
+
+        private void Accept()
+        {
+            
+            Socket client = serverSocket.Accept();
+            IPEndPoint point = client.RemoteEndPoint as IPEndPoint;
+            Console.WriteLine(point.Address + "[" + point.Port + "] connected successfully");
+            clientList.Add(client);
+
+           
+            Thread threadReceive = new Thread(Receive);
+            threadReceive.IsBackground = true;
+            threadReceive.Start(client);
+
+            Accept();
+        }
+        private void Receive(object obj)
+        {
+        
+            Socket client = obj as Socket;
+
+            IPEndPoint point = client.RemoteEndPoint as IPEndPoint;
+
+            try
+            {
+                byte[] msg = new byte[1024];
+
+                int msgLen = client.Receive(msg);
+
+                string msgStr = point.Address + "[" + point.Port + "]:" + Encoding.Default.GetString(msg, 0, msgLen);
+                Console.WriteLine(msgStr);
+
+                Broadcast(client, msgStr);
+
+                Receive(client);
+            }
+            catch
+            {
+                Console.WriteLine(point.Address + "[" + point.Port + "] is disconnected");
+                clientList.Remove(client);
+            }
+
+        }
+        private void Broadcast(Socket clientOther, string msg)
+        {
+            foreach(var client in clientList)
+            {
+                if(client == clientOther)
+                {
+
+                }
+                else
+                {
+                    client.Send(Encoding.Default.GetBytes(msg));
+                }
+            }
+        }
+    }
+}
